6231ac917eae2ff5f54501c5445647833255fbeb
michael-artes <michael.artes@outlook.com>
2015-09-30 01:22:59 -0400

    Comitando alteração

---
+++ BoaViagem/app/src/main/java/boaviagem/com/br/boaviagem/ViagemActivity.java
@@ -0,0 +1,16 @@
+package boaviagem.com.br.boaviagem;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by mmoreira on 30/09/15.
+ */
+public class ViagemActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_nova_viagem);
+    }
+}
