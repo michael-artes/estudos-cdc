6b3384be9a7b5d1f343571b3c0ec4c236d164e4e
michael-artes <michael.artes@outlook.com>
2015-09-30 01:22:24 -0400

    Commitando alterações

--- BoaViagem/app/src/main/java/boaviagem/com/br/boaviagem/DashboardActivity.java
+++ BoaViagem/app/src/main/java/boaviagem/com/br/boaviagem/DashboardActivity.java
@@ -1,6 +1,7 @@
 package boaviagem.com.br.boaviagem;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -23,5 +24,12 @@ public class DashboardActivity extends Activity {
         TextView textView = (TextView) v;
         String opcao = "Opção: " + textView.getText().toString();
         Toast.makeText(this, opcao, Toast.LENGTH_LONG).show();
+
+        switch (v.getId()){
+
+            case R.id.nova_viagem:
+                startActivity(new Intent(this, ViagemActivity.class));
+                break;
+        }
     }
 }
