a0742881e1b95af94eae33da0179adeeabc4ef7e
michael-artes <michael.artes@outlook.com>
2015-10-08 01:29:28 -0300

    Comitando a primeira vez

---
+++ teste-maior-menor/src/main/java/br/com/livro/tdd/MaiorEMenor.java
@@ -0,0 +1,33 @@
+package br.com.livro.tdd;
+
+public class MaiorEMenor {
+	
+	private Produto menor;
+	private Produto maior;
+
+	public void encontra(CarrinhoDeCompras carrinho){
+		
+		for(Produto produto : carrinho.getProdutos()) {
+			
+			if(menor == null || produto.getValor() < menor.getValor()) {
+				
+				menor = produto;
+				
+			} 
+			
+			if (maior == null ||	produto.getValor() > maior.getValor()) {
+				
+				maior = produto;
+			}
+		}
+	}
+
+	public Produto getMenor() {
+		return menor;
+	}
+
+	public Produto getMaior() {
+		return maior;
+	}
+	
+}
---
+++ teste-maior-menor/src/main/java/br/com/livro/tdd/TestaMaiorEMenor.java
@@ -0,0 +1,42 @@
+package br.com.livro.tdd;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestaMaiorEMenor {
+	
+	@Test
+	public void ordemDecrescente() {
+		
+			CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
+			
+			carrinho.adiciona(new Produto("Geladeira", 450.0));
+			carrinho.adiciona(new Produto("Liquidificador", 250.0));
+			carrinho.adiciona(new Produto("Jogo de pratos", 70.0));
+			
+			MaiorEMenor algoritmo = new MaiorEMenor();
+			algoritmo.encontra(carrinho);
+		
+			Assert.assertEquals("Jogo de pratos", algoritmo.getMenor().getNome());
+			Assert.assertEquals("Geladeira", algoritmo.getMaior().getNome());
+		
+	}
+	
+	
+	@Test
+	public void apenarUmProduto(){
+		
+		CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
+		
+		Produto produto = new Produto("Geladeira", 450.0);
+		
+		carrinho.adiciona(produto);
+		
+		MaiorEMenor algoritmo = new MaiorEMenor();
+		algoritmo.encontra(carrinho);
+	
+		Assert.assertEquals(produto, algoritmo.getMenor());
+		Assert.assertEquals(produto, algoritmo.getMaior());
+		
+	}
+}
