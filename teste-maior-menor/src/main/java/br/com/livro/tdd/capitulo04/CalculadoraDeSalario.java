8dab61abdb4187c6bb42ec9b0eebd434a4b78453
michael-artes <michael.artes@outlook.com>
2015-10-15 18:09:25 -0400

    comitando partes do capitulo 04

--- teste-maior-menor/src/main/java/br/com/livro/tdd/capitulo04/CalculadoraDeSalario.java
+++ teste-maior-menor/src/main/java/br/com/livro/tdd/capitulo04/CalculadoraDeSalario.java
@@ -2,18 +2,6 @@ package br.com.livro.tdd.capitulo04;
 
 public class CalculadoraDeSalario {
 	
-	/*public double calculaSalario(Funcionario funcionario) {
-		
-		if(funcionario.getCargo().equals(Cargo.DESENVOLVEDOR)) {
-			
-			if(funcionario.getSalario() > 3000) return 3200.0;
-			
-			return 1350.0;
-		}
-		
-		return 425.0;
-	}*/
-	
 	public double calculaSalario(Funcionario funcionario) {
 		
 		if(funcionario.getSalario() > 3000) {
@@ -23,6 +11,8 @@ public class CalculadoraDeSalario {
 		
 		return funcionario.getSalario() * 0.9;
 	}
+	
+	
 
 	
 }
