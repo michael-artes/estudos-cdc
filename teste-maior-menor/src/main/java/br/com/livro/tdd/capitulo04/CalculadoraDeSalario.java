8b9f6b4cfc0d715de2236c98f21e424feeccd87c
michael-artes <michael.artes@outlook.com>
2015-10-16 17:14:19 -0400

    Comitando alteração

--- teste-maior-menor/src/main/java/br/com/livro/tdd/capitulo04/CalculadoraDeSalario.java
+++ teste-maior-menor/src/main/java/br/com/livro/tdd/capitulo04/CalculadoraDeSalario.java
@@ -4,15 +4,31 @@ public class CalculadoraDeSalario {
 	
 	public double calculaSalario(Funcionario funcionario) {
 		
-		if(funcionario.getSalario() > 3000) {
-			
-			return funcionario.getSalario() * 0.8;
+		switch (funcionario.getCargo()) {
+			case DBA:
+				
+				if(funcionario.getSalario() < 2500) {
+					return funcionario.getSalario() * 0.85;
+				}
+				
+				return funcionario.getSalario() * 0.75;
+				
+			case DESENVOLVEDOR:
+				
+					if(funcionario.getSalario() > 3000) {
+						return funcionario.getSalario() * 0.8;
+					}
+					
+					return funcionario.getSalario() * 0.9;
+				
+			case TESTADOR:
+				
+				break;
+	
+			default: throw new RuntimeException("Funcionario invalido");
 		}
 		
-		return funcionario.getSalario() * 0.9;
+		return 0;
 	}
-	
-	
-
 	
 }
